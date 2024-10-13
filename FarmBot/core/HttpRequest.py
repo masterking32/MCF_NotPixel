3db0dd014184f3511f23a38de3ec3285623555f9
Amin.MasterkinG <lichwow_masterking@yahoo.com>
2024-10-13 13:04:11 +0330

    Add timeout for HTTP requests for very-low speed proxies.

--- FarmBot/core/HttpRequest.py
+++ FarmBot/core/HttpRequest.py
@@ -80,6 +80,7 @@ class HttpRequest:
                 url=url,
                 headers=default_headers,
                 proxies=self._get_proxy(),
+                timeout=30,
             )
 
             if response.status_code != valid_response_code:
@@ -171,6 +172,7 @@ class HttpRequest:
                 headers=default_headers,
                 data=data,
                 proxies=self._get_proxy(),
+                timeout=30,
             )
 
             if response.status_code != valid_response_code:
@@ -266,12 +268,14 @@ class HttpRequest:
                     headers=default_headers,
                     data=data,
                     proxies=self._get_proxy(),
+                    timeout=30,
                 )
             else:
                 response = requests.post(
                     url=url,
                     headers=default_headers,
                     proxies=self._get_proxy(),
+                    timeout=30,
                 )
 
             if response.status_code != valid_response_code:
@@ -348,6 +352,7 @@ class HttpRequest:
                 url=url,
                 headers=default_headers,
                 proxies=self._get_proxy(),
+                timeout=30,
             )
 
             if response.status_code != valid_response_code:
