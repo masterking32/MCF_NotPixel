2e058b9f5bd97fe6a7f07083bb51f739568374be
Amin.MasterkinG <lichwow_masterking@yahoo.com>
2024-11-02 09:14:12 +0330

    Disable Version Checker.

--- bot.py
+++ bot.py
@@ -87,7 +87,7 @@ REFERRAL_TOKEN = utilities.getConfig(
 )
 SHORT_APP_NAME = "app"
 APP_URL = None
-VERSION_HASH = "d4dcb5c5bcca1e68db9be67a5ff0ab18"
+VERSION_HASH = ""
 # End of variables to edit
 
 
